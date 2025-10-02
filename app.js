7a539d35cbd13d6bdceebcb4cafa498dccef4741
Antoine-briz <antoine.brizard@outlook.fr>
2025-10-02 20:11:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const routes = {
   "#/proba": renderProbaMenu,
   "#/proba/pneumonies": renderProbaPneumonieForm,
   "#/proba/iu": renderProbaIUForm,
+  "#/proba/abdo": renderProbaAbdoForm,
   "#/adaptee": renderAdapteeMenu,
   "#/durees": renderDureesForm
 };
