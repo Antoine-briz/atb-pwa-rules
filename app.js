451303acf83bbe478435ffa4e4c3c542d03210a1
Antoine-briz <antoine.brizard@outlook.fr>
2025-10-25 20:30:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ function renderSensiblesPage() {
   const $app = document.getElementById("app");
   $app.innerHTML = `
     <div class="bact-page">
-      <span class="title-badge">Bactéries sensibles — tableau de référence</span>
+      <span class="title-badge">Bactéries multisensibles — tableau de référence</span>
 
       <div class="info-card">
         <div class="info-content">
@@ -98,7 +98,7 @@ function renderSensiblesPage() {
                 <td>2<br><small>(mecA-)</small></td>
                 <td>2<br><small>(mecA+)</small></td>
                 <td>Céfazoline<br>Cotrimoxazole<br>Fluoroquinolones</td>
-                <td>Sensibilité préservée à l’Amoxicilline dans 10% des cas (à privilégier dans ce cas)</td>
+                <td>Sensibilité préservée à l’Amoxicilline<br>dans 10% des cas (à privilégier dans ce cas)</td>
               </tr>
               <tr>
                 <td>Staphylocoques blancs</td>
@@ -107,7 +107,7 @@ function renderSensiblesPage() {
                 <td>4</td>
                 <td>4</td>
                 <td>Linezolide<br>Daptomycine</td>
-                <td><em>S. epidermidis</em> résistant aux β-lactamines dans 70-90% des cas</td>
+                <td><em>S. epidermidis</em> résistant aux β-lactamines<br>dans 70-90% des cas</td>
               </tr>
               <tr>
                 <td><em>Streptococcus pneumoniae</em></td>
@@ -115,8 +115,8 @@ function renderSensiblesPage() {
                 <td>50-100mg/kg/24h</td>
                 <td>0,5</td>
                 <td>2</td>
-                <td>Lévofloxacine<br>Spiramycine<br><small>C3G en probabiliste, à maintenir si S. diminuée à l’Amoxicilline</small></td>
-                <td>Si méningite : Dose méningées. BP clinique Amox. à 0,5mg/L</td>
+                <td>Lévofloxacine<br>Spiramycine<br>C3G selon CMI Amox.</td>
+                <td>CMI seuil sensibilité diminuée:<br>- > 0,5mg/L ménignites<br>- > 2mg/L autres</td>
               </tr>
               <tr>
                 <td>Autres streptocoques<br><small>(Groupes A, B, C &amp; G)</small></td>
@@ -134,7 +134,7 @@ function renderSensiblesPage() {
                 <td>4</td>
                 <td>8</td>
                 <td>Vancomycine<br>Linezolide<br>Daptomycine</td>
-                <td>Résistance à l’Amoxicilline dans 0,5% des cas.<br><br>Résistant céphalosporines</td>
+                <td>Résistance à l’Amoxicilline dans 0,5% des cas.<br><br>Résistance naturelle céphalosporines (PLP5)</td>
               </tr>
               <tr>
                 <td><em>Enterococcus faecium</em></td>
@@ -143,7 +143,7 @@ function renderSensiblesPage() {
                 <td>4<br><small>(vanA-)</small></td>
                 <td>4<br><small>(vanA+)</small></td>
                 <td>Linezolide<br>Daptomycine<br>Tigécycline</td>
-                <td>Résistance à l’Amoxicilline dans 80% des cas.<br><br>Résistant céphalosporines</td>
+                <td>Résistance à l’Amoxicilline dans 80% des cas.<br><br>Résistance naturelle céphalosporines (PLP5)</td>
               </tr>
               <tr>
                 <td><em>Neisseria meningitidis</em></td>
@@ -151,7 +151,7 @@ function renderSensiblesPage() {
                 <td>150-200mg/kg/j si méningite</td>
                 <td>0,125</td>
                 <td>1</td>
-                <td>Macrolides<br>Fluoroquinolones<br>Cyclines<br>Rifampicine<br><small>C3G en probabiliste (dose méningée si méningite)</small></td>
+                <td>Macrolides<br>Fluoroquinolones<br>Cyclines<br>Rifampicine<br>C3G (en probabiliste)</td>
                 <td>Résistance naturelle au Cotrimoxazole</td>
               </tr>
               <tr>
@@ -160,7 +160,7 @@ function renderSensiblesPage() {
                 <td>50-100mg/kg/j</td>
                 <td>0,06</td>
                 <td>1</td>
-                <td><small>C3G en probabiliste</small></td>
+                <td>C3G (en probabiliste)</td>
                 <td>Résistance naturelle au Cotrimoxazole</td>
               </tr>
               <tr>
@@ -170,7 +170,7 @@ function renderSensiblesPage() {
                 <td>1</td>
                 <td>1</td>
                 <td></td>
-                <td>Pénicillinase dans 95% des cas (R. amox, S. augmentin)</td>
+                <td>Pénicillinase 95% des cas (R. amox, S. augmentin)</td>
               </tr>
               <tr>
                 <td><em>Clostridium difficile</em></td>
@@ -179,7 +179,7 @@ function renderSensiblesPage() {
                 <td>0,5</td>
                 <td>0,5</td>
                 <td>Vancomycine<br>Métronidazole<br>Tigécycline</td>
-                <td>Privilégier Fidaxomycine car moins de récurrences (Fidaxo. &gt; Vancomycine. &gt; Métronidazole)</td>
+                <td>Privilégier Fidaxomycine car moins de récurrences<br>(Fidaxo. &gt; Vancomycine. &gt; Métronidazole)</td>
               </tr>
               <tr>
                 <td><em>Listeria monocytogenes</em></td>
@@ -188,22 +188,22 @@ function renderSensiblesPage() {
                 <td>1</td>
                 <td></td>
                 <td>Cotrimoxazole<br>Macrolides<br>Moxifloxacine<br>Linezolide</td>
-                <td>Si inf. neuroméningées : Cotrimoxazole, Moxifloxacine et Linezolide possibles<br><br>Résistant céphalosporines</td>
+                <td>Si inf. neuroméningées : Cotrimoxazole, Moxifloxacine<br>et Linezolide possibles<br><br>Résistant céphalosporines</td>
               </tr>
               <tr>
-                <td><em>Nocardia</em> spp.<br><small>N. farcinica / N. abscessus / N. asteroides / N. nova</small></td>
+                <td><em>Nocardia</em> spp.<br><small>N. farcinica<br>N. abscessus<br>N. asteroides<br>N. nova</small></td>
                 <td>Cotrimoxazole IV<br><small>en bithérapie avec :</small><br>– Ou Imipénème<br>– Ou Linezolide<br>– Ou Amikacine</td>
                 <td>20+100mg/kg/j<br><small>Imipénème 3-4g/24g</small><br><small>Linezolide 600mg x2/j</small><br><small>Amikacine 25-30mg/kg</small></td>
                 <td>4<br>8<br>8<br>2/38</td>
                 <td>16<br>64<br>16<br>4/76</td>
                 <td></td>
-                <td>Pneumonies non graves : une monothérapie par cotrimoxazole ou linezolide est possible. Le choix de la bi-thérapie dépend de l’espèce identifiée (N. farcinica en majorité).<br><br>Activité quasi constante (&gt;95%) de cotrimoxazole, linezolide et amikacine.</td>
+                <td>Monothérapie possible (Pneumonies non graves)<br>Choix de la bi-thérapie selon espèce.<br><br>Activité quasi constante (&gt;95%) de cotrimoxazole,<br>linezolide et amikacine.</td>
               </tr>
 
               <!-- ===== Partie 2 du tableau ===== -->
 
               <tr>
-                <td>Entérobactéries Groupe 0<br><small><em>Proteus mirabilis</em>, <em>Salmonella</em> spp.</small></td>
+                <td>Entérobactéries Groupe 0<br><small><em>Proteus mirabilis</em>,<br><em>Salmonella</em> spp.</small></td>
                 <td>Amoxicilline</td>
                 <td>50-100mg/kg/j</td>
                 <td>1</td>
@@ -212,16 +212,16 @@ function renderSensiblesPage() {
                 <td>Résistances C3G : BLSE (CTX-M) &gt; AmpC plasmidiques</td>
               </tr>
               <tr>
-                <td>Entérobactéries Groupe 1<br><small><em>Escherichia coli</em>, <em>Shigella</em></small></td>
+                <td>Entérobactéries Groupe 1<br><small><em>Escherichia coli</em>,<br><em>Shigella</em></small></td>
                 <td>Amoxicilline</td>
                 <td>50-100mg/kg/j</td>
                 <td>1</td>
                 <td>4</td>
                 <td>C3G<br>Aztréonam<br>Ciprofloxacine<br>Cotrimoxazole</td>
-                <td>Résistances C3G : BLSE (CTX-M) &gt; AmpC déréprimée &gt; AmpC plasmidique</td>
+                <td>Résistances C3G : BLSE (CTX-M) &gt; AmpC déréprimée<br>&gt; AmpC plasmidique</td>
               </tr>
               <tr>
-                <td>Entérobactéries Groupe 2<br><small><em>K. pneumoniae</em>, <em>Citrobacter koserii</em></small></td>
+                <td>Entérobactéries Groupe 2<br><small><em>K. pneumoniae</em>,<br><em>K. oxytoca</em>,<br><em>Citrobacter koserii</em></small></td>
                 <td>Amoxicilline-Clav.</td>
                 <td>1g/0,5g x3/24h</td>
                 <td>1</td>
@@ -230,13 +230,13 @@ function renderSensiblesPage() {
                 <td>Résistances C3G : BLSE (CTX-M) &gt; AmpC plasmidiques</td>
               </tr>
               <tr>
-                <td>Entérobactéries Groupe 3<br><small><em>E. cloacae</em>, <em>K. aerogenes</em>, <em>C. freundii</em>, <em>M. morganii</em>, <em>S. marcescens</em>, <em>H. alvei</em>, <em>Providencia</em></small></td>
+                <td>Entérobactéries Groupe 3<br><small><em>E. cloacae</em>,<br><em>K. aerogenes</em>,<br><em>C. freundii</em>,<br><em>M. morganii</em>,<br><em>S. marcescens</em>,<br><em>H. alvei</em>,<br><em>Providencia</em></small></td>
                 <td>Céfépime</td>
                 <td>4-6g/24h</td>
                 <td>1</td>
                 <td>4</td>
-                <td>Ciprofloxacine<br>Cotrimoxazole<br>Tigécycline (sauf protéacées : <em>M. morganii</em>, <em>Providencia</em>)</td>
-                <td>Résistances C3G : AmpC induite &gt; BLSE &gt; AmpC déréprimée<br><br><em>E. cloacae</em>, <em>K. aerogenes</em>, <em>C. freundii</em> : pas de C3G ou Tazocilline (risque AmpC induite)<br><br><em>S. marcescens</em> résistant naturellement à l’Amikacine</td>
+                <td>Ciprofloxacine<br>Cotrimoxazole<br>Tigécycline (sauf protéacées :<br><em>M. morganii</em>, <em>Providencia</em>)</td>
+                <td>Résistances C3G : AmpC induite &gt; BLSE &gt; AmpC déréprimée<br><br><em>E. cloacae</em>, <em>K. aerogenes</em>, <em>C. freundii</em> : pas de C3G ou<br>Tazocilline (risque AmpC induite)<br><br><em>S. marcescens</em> résistant naturellement à l’Amikacine</td>
               </tr>
               <tr>
                 <td><em>Pseudomonas aeruginosa</em></td>
@@ -245,7 +245,7 @@ function renderSensiblesPage() {
                 <td>0,001<br>0,001</td>
                 <td>8<br>16</td>
                 <td>Céfépime<br>Aztréonam<br>Ciprofloxacine<br>Carbapénèmes<br>Amikacine</td>
-                <td>Résistances : Pase, Case AmpC, BLSE, PorineD2, carbapénèmases, efflux MexAB/XY-OprM<br><br>Résistance naturelle à la Tigécycline.</td>
+                <td>Résistances : Pase, Case AmpC, BLSE, PorineD2,<br>carbapénèmases, efflux MexAB/XY-OprM<br><br>Résistance naturelle à la Tigécycline.</td>
               </tr>
               <tr>
                 <td><em>Acinetobacter baumannii</em></td>
@@ -254,7 +254,7 @@ function renderSensiblesPage() {
                 <td>2<br>4</td>
                 <td>8<br>8</td>
                 <td>Ciprofloxacine<br>Cotrimoxazole<br>Colistine<br>Tigécycline</td>
-                <td>Résistances : Pase, Case AmpC, BLSE, carbapénèmases (ABRI), efflux AdeABC, Acétylase AAC-6’</td>
+                <td>Résistances : Pase, Case AmpC, BLSE,<br>carbapénèmases (ABRI), efflux AdeABC, Acétylase AAC-6’</td>
               </tr>
               <tr>
                 <td><em>Stenotrophomonas maltophilia</em></td>
